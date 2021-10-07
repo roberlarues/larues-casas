1634592c67b847cfc9bda0b71c7bd883761e5357
Roberto Clemente <rclementesalvador@gmail.com>
2021-10-08 00:37:49 +0200

    Adaptación responsive

--- src/components/HouseDetail.tsx
+++ src/components/HouseDetail.tsx
@@ -5,17 +5,20 @@ import './HouseDetail.css';
 export function HouseDetail({ house, closeModal }) {
     return (
         <div className="HouseDetail">
-            <div className="house-detail-item">
-                <img src={process.env.PUBLIC_URL + '/images/' + house.images[0]} alt="Sin imagen" width="100%"/>
-            </div>
-            <div className="house-detail-item">
-                <h1>{house.name}</h1>
-                <p>{house.description}</p>
-            </div>
 
             <button className="close-button" onClick={closeModal}>
                 Cerrar
             </button>
+
+            <div className="house-detail-content">
+                <div className="house-detail-item">
+                    <img src={process.env.PUBLIC_URL + '/images/' + house.images[0]} alt="Sin imagen"/>
+                </div>
+                <div className="house-detail-item">
+                    <h1>{house.name}</h1>
+                    <p className="house-detail-description">{house.description}</p>
+                </div>
+            </div>
         </div>
     );
 }
